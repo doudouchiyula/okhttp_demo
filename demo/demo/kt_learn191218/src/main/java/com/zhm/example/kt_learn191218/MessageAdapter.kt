4b9397b2304bf2f92f995e5b2b19d748cfacb82e
zhaohuimin <1045005893@qq.com>
2019-12-23 23:07:19 +0800

    adapter实现

--- demo/demo/kt_learn191218/src/main/java/com/zhm/example/kt_learn191218/MessageAdapter.kt
+++ demo/demo/kt_learn191218/src/main/java/com/zhm/example/kt_learn191218/MessageAdapter.kt
@@ -1,27 +1,50 @@
 package com.zhm.example.kt_learn191218
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class MessageAdapter : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
+class MessageAdapter(context: Context, list: MutableList<MessageBean>?) : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
+
+    private var alist: MutableList<MessageBean>
+    private var context: Context
+
+    init {
+        this.alist = list ?: mutableListOf()
+        this.context = context
+
+    }
+
+    fun resetData(list: List<MessageBean>) {
+        alist.clear()
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageAdapter.ViewHolder {
         val inflate = LayoutInflater.from(parent.context).inflate(R.layout.item_message, null)
-        ViewHolder(inflate)
         return ViewHolder(inflate)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return alist.size
     }
 
+
     override fun onBindViewHolder(holder: MessageAdapter.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val messageBean = alist.get(position)
+        holder.tvContent.setText(messageBean.content)
+        holder.tvTag.setText(messageBean.tag)
+        Glide.with(context).load(messageBean.picUrl).into(holder.ivIcon)
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        val tvTag: TextView by lazy { itemView.findViewById<TextView>(R.id.tv_tag) }
+        val tvContent: TextView by lazy { itemView.findViewById<TextView>(R.id.tv_content) }
+        val ivIcon: ImageView by lazy { itemView.findViewById<ImageView>(R.id.iv_icon) }
     }
 }
---
+++ demo/demo/kt_learn191218/src/main/java/com/zhm/example/kt_learn191218/ResultUtil.kt
@@ -0,0 +1,8 @@
+package com.zhm.example.kt_learn191218
+
+open class ResultUtil<T> {
+    val code: Int = -6666
+    var msg: String = ""
+    var data: T? = null
+
+}
